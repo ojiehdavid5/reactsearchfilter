49f23b45a73079fc51b998804f8d3f535f3111aa
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-03-08 00:08:32 +0100

    changes

--- src/AddItem.js
+++ src/AddItem.js
@@ -3,6 +3,7 @@ import {useRef} from 'react'
 import {FaPlus} from 'react-icons/fa';
 
 const AddItem = ({newItem,setNewItem,handleSubmit}) => {
+    const inputRef =useRef();
   return (
     <form className='addForm' onSubmit={handleSubmit}>
         <label 
@@ -14,6 +15,7 @@ const AddItem = ({newItem,setNewItem,handleSubmit}) => {
         </label>
         <input
         autoFocus
+        ref={inputRef}
         id='addItem'
         type='text'
         placeholder='Add Item'
@@ -26,6 +28,8 @@ const AddItem = ({newItem,setNewItem,handleSubmit}) => {
         <button
         type='submit'
         aria-label='Add Item'
+        onClick={()=>inputRef.current.focus()}
+
         >
             <FaPlus/>
         </button>
