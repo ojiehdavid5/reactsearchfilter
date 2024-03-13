fa7541b780be25b0486857a7a144bd7fa022c0bb
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-03-13 22:46:12 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -72,9 +72,17 @@ const handleCheck= async(id)=>{
 
 }
 
-const handleDelete=(id)=>{
+const handleDelete= async (id)=>{
   const listItems=items.filter((item)=> item.id!==id)
   setItems(listItems)
+  
+  const deleteOptions={
+    method:'DELETE',
+
+  };
+  const reqUrl=`${API_URL}/${id}`
+  const result= await apiRequest(reqUrl,deleteOptions);
+   if (result) setFetchError(result);
 
 }
 
